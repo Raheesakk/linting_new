6521100753529fadcf32cdb42448cdb51987a759
Raheesa <fathimaraheesakk@gmail.com>
2025-09-14 16:11:37 +0530

    deleted string statements

--- test_greet.py
+++ test_greet.py
@@ -5,6 +5,5 @@ class TestGreet(unittest.TestCase):
     """Greeter class provides a method to greet users."""
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World!")
-        """Test that greet() returns the correct formatted string."""
 if __name__ == '__main__':
     unittest.main()
